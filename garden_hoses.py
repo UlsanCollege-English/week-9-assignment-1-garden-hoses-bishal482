1fa9140b325788cf0e6d384ef850977e73291db1
bishal482 <galanbishal20@gmail.com>
2025-10-30 19:23:40 +0900

    Are the integer correct for the expected result? Done many tryings, but nothing worked.

--- garden_hoses.py
+++ garden_hoses.py
@@ -38,3 +38,24 @@ if __name__ == "__main__":
     print(min_cost_connect([5,2,4]))     # 18
     print(min_cost_connect([8,4,6,12]))  # 58
     print(min_cost_connect([20,4,8,2]))  # 54
+
+# PS C:\Users\galan\ben_python\week-9-assignment-1-g
+# PS C:\Users\galan\ben_python\week-9-assignment-1-garden-hoses-bishal482> pytest -q                  
+                                                  
+# ==================== ERRORS =====================
+# ______ ERROR collecting tests/test_hw01.py ______
+# tests\test_hw01.py:6: in <module>
+#     main = importlib.util.module_from_spec(SPEC); SPEC.loader.exec_module(main)
+#                                                   ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+# <frozen importlib._bootstrap_external>:1026: in exec_module
+#     ???
+# <frozen importlib._bootstrap>:488: in _call_with_frames_removed
+#     ???
+# garden_hoses.py:41: in <module>
+#     sdfs
+# E   NameError: name 'sdfs' is not defined
+# ============ short test summary info ============ 
+# ERROR tests/test_hw01.py - NameError: name 'sdfs' is not defined
+# !!!! Interrupted: 1 error during collection !!!!! 
+# 1 error in 0.22s
+# PS C:\Users\galan\ben_python\week-9-assignment-1-garden-hoses-bishal482>
